b9f87ddaf624300565844dd8bd6c7ae3641b0cdb
Andrii Kravtsov <kravtsov798@gmail.com>
2025-07-14 22:04:25 +0200

    add Constants

---
+++ TransactionsTestTask/App/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  TransactionsTestTask
+//
+//  Created by Andrii Kravtsov on 14.07.2025.
+//
+
+import Foundation
+
+enum Constants {
+    enum Layout {
+        static let hPadding: CGFloat = 16
+        static let interItemSpacing: CGFloat = 8
+        static let buttonHeight: CGFloat = 44
+        static let textFieldHeight: CGFloat = 60
+    }
+}
